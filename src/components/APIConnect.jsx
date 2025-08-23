42a8731684b96df5af33fe751647aac80ae91206
shantanu1235 <shantanunigam786@gmail.com>
2025-08-23 23:37:44 +0530

    first commi25

--- src/components/APIConnect.jsx
+++ src/components/APIConnect.jsx
@@ -1,4 +1,4 @@
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'https://ecom-backend-4-bdss.onrender.com/api/auth'
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'https://ecom-backend-5-9wbn.onrender.com/api/auth'
 
 
 // General function to get full API endpoint
