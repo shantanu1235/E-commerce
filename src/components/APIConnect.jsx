0b71bc78e748544ffd2af7870358903435d47186
shantanu1235 <shantanunigam786@gmail.com>
2025-08-23 13:05:09 +0530

    first commi22

--- src/components/APIConnect.jsx
+++ src/components/APIConnect.jsx
@@ -1,4 +1,5 @@
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:5002/api/auth";
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'https://ecom-backend-2-754w.onrender.com/api/auth'
+
 
 // General function to get full API endpoint
 export  function getApiEndpoint(route) {
